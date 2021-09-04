56975b2f101315d9ee5bf1129d9a7f770f28cb6f
GuilhermeBrant <brant.guime@gmail.com>
2021-09-03 22:20:13 -0300

    adjust function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
 // JavaScript Document
 
-function insert(num)
-{
-    var numero = document.getElementById('visualizacao').innerHTML;
-    document.getElementById('visualizacao').innerHTML = numero + num;
-}
-
-function clean()
-{
-    document.getElementById('visualizacao').innerHTML = "";
-}
-
 function back()
 {
     var resultado = document.getElementById('visualizacao').innerHTML;
     document.getElementById('visualizacao').innerHTML = resultado.substring(0, resultado.length -1);
 }
+
+function insert(num)
+{
+    var numero = document.getElementById('visualizacao').innerHTML
+    if (numero === '0' && num !== '.') {
+        document.getElementById('visualizacao').innerHTML = numero.substring(0, numero.length -1) + num
+    } else {
+    document.getElementById('visualizacao').innerHTML = numero + num;
+    }
+};
+
+function clean()
+{
+    document.getElementById('visualizacao').innerHTML = "0";
+}
+
+
 
 function calcular()
 {
